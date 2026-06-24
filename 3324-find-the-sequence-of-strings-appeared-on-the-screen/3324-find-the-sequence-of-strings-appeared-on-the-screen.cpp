90510bd49f8fb8d620c8201b2188bd0aa61aee15
P1yush21 <piyushsharma21246@gmail.com>
2026-06-24 18:45:26 +0530

    Time: 12 ms (94.68%), Space: 95.1 MB (35.55%) - LeetHub

---
+++ 3324-find-the-sequence-of-strings-appeared-on-the-screen/3324-find-the-sequence-of-strings-appeared-on-the-screen.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<string> stringSequence(string target) {
+        int n = target.size();
+        vector<string> str;
+        string ans;
+        for(int i = 0; i < n; i++){
+            ans.push_back('a');   
+            str.push_back(ans);
+            char ch = 'a';
+            if(ch < target[i]){
+                while(ch < target[i]){
+                    ch++;
+                    ans[i]++;
+                    str.push_back(ans);
+                }
+            }
+        }
+        return str;
+    }
+};
