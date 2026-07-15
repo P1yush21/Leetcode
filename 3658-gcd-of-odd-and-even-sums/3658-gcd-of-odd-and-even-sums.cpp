e069ca28c9871ff908fcd26d7b45365e21d49ef0
P1yush21 <piyushsharma21246@gmail.com>
2026-07-15 06:10:46 +0530

    Time: 571 ms (7.01%), Space: 8.7 MB (9.11%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int gcd(int nums1, int nums2){
-        for(int i = max(nums1,nums2)/2; i >= 0; i--){
+        for(int i = min(nums1,nums2); i >= 1; i--){
             if(nums1%i==0 && nums2%i == 0) return i;
         }
-        return 0;
+        return 1;
     }
     int gcdOfOddEvenSums(int n) {
-        int sumEven = 0, oddSum = 0;
-        for(int i = 0; i <= 2*n; i++){
-            if(i%2==0) sumEven += i;
-            else oddSum += i;
-        }
+        // int sumEven = 0, oddSum = 0;
+        // for(int i = 0; i <= 2*n; i++){
+        //     if(i%2==0) sumEven += i;
+        //     else oddSum += i;
+        // }
+        int sumEven = n*(n+1);
+        int oddSum = n*n;
         return gcd(sumEven, oddSum);
     }
 };
