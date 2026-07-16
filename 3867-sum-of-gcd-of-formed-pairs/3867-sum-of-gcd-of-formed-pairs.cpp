4ee3f4690ecefeba799d1140d9c24837fed343ef
P1yush21 <piyushsharma21246@gmail.com>
2026-07-16 07:36:11 +0530

    Time: 74 ms (59.14%), Space: 161.1 MB (56.07%) - LeetHub

---
+++ 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int gcd(int num1, int num2){
+        // for(int i = min(num1,num2); i >= 1; i--){
+        //     if(num1%i==0 && num2%i==0) return i;
+        // }
+        // return 1;
+        if(num2 == 0) return num1;
+        return gcd(num2, num1 % num2);
+    }
+    long long gcdSum(vector<int>& nums) {
+        int n = nums.size();
+        long long mx = nums[0];
+        vector<long long>prefix(n);
+        prefix[0] = nums[0];
+        for(int i = 1; i < n; i++){
+            if(nums[i]>mx){
+                mx = nums[i];
+            }
+            prefix[i] = gcd(mx,nums[i]);
+        }
+        cout<<12%18;
+        sort(prefix.begin(),prefix.end());
+        long long sum = 0;
+        int i = 0;
+        int j = n-1;
+        while(i < j){
+            sum += gcd(prefix[i],prefix[j]);
+            i++;
+            j--;
+        }
+        return sum;
+    }
+};
