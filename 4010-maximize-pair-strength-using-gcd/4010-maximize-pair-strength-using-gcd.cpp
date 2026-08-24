13a98f4f36584e7610bdd6c5bf9ebe06ec9e0f05
P1yush21 <piyushsharma21246@gmail.com>
2026-08-24 07:29:07 +0530

    Time: 747 ms (76.5%), Space: 37.3 MB (15.43%) - LeetHub

--- 4010-maximize-pair-strength-using-gcd/4010-maximize-pair-strength-using-gcd.cpp
+++ 4010-maximize-pair-strength-using-gcd/4010-maximize-pair-strength-using-gcd.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         long long ans = 0;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = i+1; j < n; j++) {
                 long long g = gcd(nums[i], nums[j]);
 
                 long long strength = (1LL * nums[i] * nums[j]) / (g * g);
