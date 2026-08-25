c88945a77b2f436bda0f6172ff5c2db8c2ffe9aa
P1yush21 <piyushsharma21246@gmail.com>
2026-08-25 06:31:01 +0530

    Time: 4 ms (23.25%), Space: 24 MB (94.59%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -2,25 +2,25 @@ class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
         int n = nums.size();
-        sort(nums.begin(),nums.end());
+        // sort(nums.begin(),nums.end());
         for(int i = 1; i < 101; i++){
             int mul = k*i;
             bool flag = false;
-            // for(int j = 0; j < n; j++){
-            //     if(mul==nums[j]) flag = true;
-            // }
-            // if(!flag) return k*i;
-            int low = 0, high = n-1;
-            while(low<=high){
-                int mid = low + (high-low)/2;
-                if(mul==nums[mid]) {
-                    flag=true;
-                    break;
-                }
-                else if(mul>nums[mid]) low = mid+1;
-                else high=mid-1;
+            for(int j = 0; j < n; j++){
+                if(mul==nums[j]) flag = true;
             }
             if(!flag) return k*i;
+            // int low = 0, high = n-1;
+            // while(low<=high){
+            //     int mid = low + (high-low)/2;
+            //     if(mul==nums[mid]) {
+            //         flag=true;
+            //         break;
+            //     }
+            //     else if(mul>nums[mid]) low = mid+1;
+            //     else high=mid-1;
+            // }
+            // if(!flag) return k*i;
         }
         return nums[n-1]+1;
     }
