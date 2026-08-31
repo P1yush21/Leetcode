8f051cd48f4849eb5b1a557b3e80721ffb5c48ae
P1yush21 <piyushsharma21246@gmail.com>
2026-08-31 18:21:30 +0530

    Time: 0 ms (100%), Space: 11.9 MB (26.03%) - LeetHub

---
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minLength(string s) {
+        int n = s.size();
+        stack<char>st;
+        for(int i = 0; i < n; i++){
+            if(st.size()>0 && ((st.top()=='A' && s[i]=='B') || (st.top()=='C' && s[i]=='D'))) st.pop();
+            else st.push(s[i]);
+        }
+        return st.size();
+    }
+};
