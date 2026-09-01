dc53d442edb2862d336629fc30a39dc7d9b24ffc
P1yush21 <piyushsharma21246@gmail.com>
2026-09-01 21:01:22 +0530

    Time: 1 ms (54.35%), Space: 12.5 MB (81.99%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>v;
         int n = nums1.size();
         int m = nums2.size();
-        for(int i = 0; i < n; i++){
-            int idx = -1;
-            int mx = -1;
-            for(int j = 0; j < m; j++){
-                if(nums1[i]==nums2[j]) {
-                    idx=j;
-                    break;
-                }
-            }
-            for(int k = idx+1; k < m; k++){
-                if(nums2[k]>nums1[i]){
-                    mx = nums2[k];
-                    break;
-                }
-            }
-            v.push_back(mx);
+        stack<int>st;
+        vector<int>v(m);
+        st.push(v[m-1]);
+        for(int i = m-1; i >= 0; i--){
+            while(st.size()>0 && st.top()<=nums2[i]) st.pop();
+
+            if(st.size()==0) v[i]=-1;
+            else v[i] = st.top();
+
+            st.push(nums2[i]);
         }
-        return v;
+        vector<int>ans;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(nums1[i]==nums2[j]){
+                    ans.push_back(v[j]);
+                    break;
+                }
+            }
+        }
+        return ans;
     }
 };
