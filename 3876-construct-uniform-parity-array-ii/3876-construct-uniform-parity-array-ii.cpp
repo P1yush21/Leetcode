769d525ea1a6a234cc2d5d7efa1a067237ccc655
P1yush21 <piyushsharma21246@gmail.com>
2026-09-03 10:51:43 +0530

    Time: 2718 ms (5.24%), Space: 173.8 MB (11.52%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>v(n);
+        int odd = 0, even = 0;
+        int minEven = INT_MAX, minOdd = INT_MAX;
+        for(int i = 0; i < n; i++){
+            if(nums[i]%2==0) {
+                even++;
+                minEven = min(minEven, nums[i]);
+            }
+            else {
+                odd++;
+                minOdd = min(minOdd, nums[i]);
+            }
+        }
+        cout<<minEven<<" "<<minOdd<<endl;
+        for(int i = 0; i < n; i++){
+            if(nums[i]%2==0 && minEven>minOdd) {
+                v[i] = minEven-minOdd;
+            }
+            else {
+                v[i] = nums[i];
+            }
+        }
+        for(int i = 0; i < n; i++){
+            cout<<v[i]<<" ";
+        }
+        odd = 0, even = 0;
+        for(int i = 0; i < n; i++){
+            if(v[i]%2==0) {
+                even++;
+            }
+            else {
+                odd++;
+            }
+        }
+        if(odd==n || even==n)return true;
+        return false;
+    }
+};
