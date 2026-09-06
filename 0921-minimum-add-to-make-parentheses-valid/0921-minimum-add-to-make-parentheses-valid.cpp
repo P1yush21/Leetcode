05e784f95249031d3217223f3c176ea91f073c0b
P1yush21 <piyushsharma21246@gmail.com>
2026-09-06 09:15:06 +0530

    Time: 0 ms (100%), Space: 9.8 MB (7.23%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
+        // int n = s.size();
+        // int open = 0;
+        // int close = 0;
+        // for(int i = 0; i < n; i++){
+        //     if(s[i]=='(') open++;
+        //     else{
+        //         if(open > 0) open--;
+        //         else close++;
+        //     }
+        // }
+        // return open + close;
+
         int n = s.size();
-        int open = 0;
-        int close = 0;
+        stack<int>st;
         for(int i = 0; i < n; i++){
-            if(s[i]=='(') open++;
+            if(s[i]=='(') st.push(s[i]);
             else{
-                if(open > 0) open--;
-                else close++;
+                if(st.size()>0 && st.top()=='(' && s[i]==')') st.pop();
+                else st.push(s[i]);
             }
         }
-        return open + close;
+        return st.size();
     }
 };
