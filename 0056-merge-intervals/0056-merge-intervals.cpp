5ab36080584f447da6d12a37d186de7750d9d94e
Piyush Sharma <piyushsharma21246@gmail.com>
2026-09-11 23:13:50 +0530

    Time: 2 ms (92.81%), Space: 24 MB (33.09%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,17 +4,26 @@ public:
         int n = nums.size();
         vector<vector<int>>ans;
         sort(nums.begin(),nums.end());
+        // for(int i = 0; i < n; i++){
+        //     int start = nums[i][0];
+        //     int end = nums[i][1];
+        //     if(ans.size()>0 && end <= ans.back()[1]) continue;
+        //     for(int j = i+1; j < n; j++){
+        //         if(end >= nums[j][0]){
+        //             end = max(end, nums[j][1]);
+        //         }
+        //         else break;
+        //     }
+        //     ans.push_back({start,end});
+        // }
+
         for(int i = 0; i < n; i++){
-            int start = nums[i][0];
-            int end = nums[i][1];
-            if(ans.size()>0 && end <= ans.back()[1]) continue;
-            for(int j = i+1; j < n; j++){
-                if(end >= nums[j][0]){
-                    end = max(end, nums[j][1]);
-                }
-                else break;
+            if(ans.size()==0 || nums[i][0] > ans.back()[1]){
+                ans.push_back(nums[i]);
             }
-            ans.push_back({start,end});
+            else{
+                ans.back()[1] = max(ans.back()[1], nums[i][1]);
+            }
         }
 
         return ans;
